ea1f95edcbcafa63e86f5e5975259087caee978c
bexpe <beata.k.puzio@gmail.com>
2018-03-14 16:17:32 +0100

    Add validation delete while loop

--- game_class.py
+++ game_class.py
@@ -9,55 +9,62 @@ class Game:
         self.game_id = 1
 
     def run(self):
+        words = ['sun', 'holidays', 'summertime', 'fun', 'wind']
+        print("Welcome in hangman game!")
+        user_name = input("Type your name\n")
+        new_player = player_class.Player(user_name)
+        secret = random.choice(words)
+        print(secret)
+        correct_guesses = set()
+        incorrect_guesses = set()
+
         while True:
-            words = ['sun', 'holidays', 'summertime', 'fun', 'wind']
-            print("Welcome in hangman game!")
-            user_name = input("Type your name\n")
-            new_player = player_class.Player(user_name)
-            secret = random.choice(words)
-            print(secret)
-            correct_guesses = set()
-            incorrect_guesses = set()
+            print("\nLives: " + str(new_player.lives))
+            guess = input("Type a letter u want to guess:\n")
+            if guess.isdigit():
+                print("You cant type a number")
+                continue
 
-            while True:
-                print("Lives: " + str(new_player.lives))
-                guess = input("Type a letter u want to guess:\n")
+            if guess in correct_guesses & incorrect_guesses:
+                print('You already guessed that letter')
 
-                if guess in correct_guesses & incorrect_guesses:
-                    print('You already guessed that letter')
-
-                elif guess in secret:
-                    # correct guess!
-                    correct_guesses.add(guess)
-                    display_word = ''.join(char if char in correct_guesses else '_' for char in secret)
-                    new_player.score += 10
-                    print(display_word)
-                    if display_word == secret:
-                        print("Winner!!!")
-                        sqlraw.connect_to_db(new_player)
-                        play_again = input("Do you want to play again? y/n")
-                        if play_again.lower() == "y":
-                            continue
-                        elif play_again.lower() == "n":
-                            break
-                        else:
-                            print("Wrong answer")
-                            continue
-                else:
-                    # incorrect guess!
-                    incorrect_guesses.add(guess)
-                    print('Oops, incorrect guess!')
-                    if new_player.lives > 1:
-                        new_player.lives -= 1
+            elif guess in secret:
+                # correct guess!
+                correct_guesses.add(guess)
+                display_word = ''.join(char if char in correct_guesses else '_' for char in secret)
+                new_player.score += 10
+                print(display_word)
+                if display_word == secret:
+                    print("Winner!!!")
+                    sqlraw.connect_to_db(new_player)
+                    play_again = input("Do you want to play again? y/n\n")
+                    if play_again.lower() == "y":
+                        break
+                    elif play_again.lower() == "n":
+                        quit()
                     else:
-                        print("Game over")
-                        play_again = input("Do you want to play again? y/n")
-                        if play_again.lower() == "y":
-                            continue
-                        elif play_again.lower() == "n":
-                            break
-                        else:
-                            print("Wrong answer")
-                            continue
-                print("Correct guesses: " + str(correct_guesses))
-                print("Incorrect guesses: " + str(incorrect_guesses))
+                        print("Wrong answer\n")
+                        quit()
+            else:
+                # incorrect guess!
+                incorrect_guesses.add(guess)
+                print('Oops, incorrect guess!\n')
+                if new_player.lives > 1:
+                    new_player.lives -= 1
+                else:
+                    print("\nThe correct answer was: " + secret)
+                    print("\nGame over")
+                    play_again = input("Do you want to play again? y/n\n")
+                    if play_again.lower() == "y":
+                        break
+                    elif play_again.lower() == "n":
+                        quit()
+                    else:
+                        print("Wrong answer\n")
+                        quit()
+            print("Correct guesses: ")
+            for i in correct_guesses:
+                print(i, end=" ")
+            print("Incorrect guesses: ")
+            for i in incorrect_guesses:
+                print(i, end=" ")
