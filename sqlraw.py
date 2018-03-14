543f10c360ed18bbc843b01aeea518503db65d89
bexpe <beata.k.puzio@gmail.com>
2018-03-14 15:26:46 +0100

    Create add to winners method

--- sqlraw.py
+++ sqlraw.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
-
+import game_class
 # utworzenie połączenia z bazą przechowywaną na dysku
 # lub w pamięci (':memory:')
 
 
-def connect_to_db():
+def connect_to_db(player):
     connection = sqlite3.connect('game.db')
     connection.row_factory = sqlite3.Row
     # acces to columns through indexes and names
     cursor = connection.cursor()  # object of cursor created
     create_main_table(cursor)
     create_sub_table(cursor)
-    insert_data_into_table(cursor, connection)
+    add_to_winners(player, cursor, connection)
     read_data_from_db(cursor)
 
 
@@ -44,7 +44,8 @@ def insert_data_into_table(cursor, connection):
     cursor.execute('INSERT INTO game VALUES(NULL, ?);', ('hangman',))
     # remember about coma after hangman, its how we create one-piece tuple
     cursor.execute('SELECT id FROM game WHERE game_name = ?', ('hangman',))
-    game_id = cursor.fetchone()[0]
+    game_class.Game.game_id = cursor.fetchone()[0]
+    print(game_class.Game.game_id)
     players = (
         (None, 'Tomasz', 250, game_id),
         (None, 'hangmanger', 1500, game_id),
@@ -55,17 +56,27 @@ def insert_data_into_table(cursor, connection):
     connection.commit()
 
 
+def add_to_winners(player, cursor, connection):
+    cursor.execute('INSERT INTO game VALUES(NULL, ?);', ('hangman',))
+    cursor.execute('SELECT id FROM game WHERE game_name = ?', ('hangman',))
+    game_id = cursor.fetchone()[0]
+    players = (
+        (None, player.name, player.score, game_id),
+        (None, 'hangmanger', 1500, game_id),
+        (None, 'Piotr', 5, game_id)
+    )
+    cursor.executemany('INSERT INTO player VALUES(?,?,?,?)', players)
+    connection.commit()
+
+
 def read_data_from_db(cursor):
     """Function dowloads and prints data form database."""
     cursor.execute(
         """
         SELECT player.id,name,score FROM player,game
-        WHERE player.game_id=game.id
+        WHERE player.game_id=game.id ORDER BY score DESC
         """)
     players = cursor.fetchall()
     for player in players:
-        print(player['id'], player['name'], player['score'])
+        print(player['score'], player['name'])
     print()
-
-
-connect_to_db()
