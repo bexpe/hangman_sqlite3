045c727b4daaae157fb56780f584ff1fd687ef7c
bexpe <beata.k.puzio@gmail.com>
2018-03-15 14:39:47 +0100

    Delete useless comments

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ def main():
     while True:
         new_game = game_class.Game()
         new_game.run()
-    # nie dziala jeszcze:
-    # w bazie nie zapisuje sie trwale wynik
 
 
 if __name__ == '__main__':
