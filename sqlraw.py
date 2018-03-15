14c1731017448fc9b438310039ac08fb5e413982
bexpe <beata.k.puzio@gmail.com>
2018-03-15 14:41:00 +0100

    Add delete and update methods, add limit in scores

--- sqlraw.py
+++ sqlraw.py
@@ -17,7 +17,7 @@ def connect_to_db(player):
 
 
 def create_main_table(cursor):
-    cursor.execute("DROP TABLE IF EXISTS game;")
+    # cursor.execute("DROP TABLE IF EXISTS game;")
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS game (
@@ -28,7 +28,6 @@ def create_main_table(cursor):
 
 def create_sub_table(cursor):
     cursor.executescript("""
-        DROP TABLE IF EXISTS player;
         CREATE TABLE IF NOT EXISTS player (
             id INTEGER PRIMARY KEY ASC,
             name varchar(250) NOT NULL,
@@ -44,8 +43,6 @@ def add_to_winners(player, cursor, connection):
     game_id = cursor.fetchone()[0]
     players = (
         (None, player.name, player.score, game_id),
-        (None, 'hangmanger', 1500, game_id),
-        (None, 'Piotr', 5, game_id)
     )
     cursor.executemany('INSERT INTO player VALUES(?,?,?,?)', players)
     connection.commit()
@@ -56,10 +53,23 @@ def read_data_from_db(cursor):
     cursor.execute(
         """
         SELECT player.id,name,score FROM player,game
-        WHERE player.game_id=game.id ORDER BY score DESC
+        WHERE player.game_id=game.id ORDER BY score DESC LIMIT 10
         """)
     players = cursor.fetchall()
-    print("***********The best scores************")
+    print("********10 The best scores*********")
+    print("")
     for player in players:
         print(player['score'], player['name'])
     print()
+
+
+def update_data_in_db():
+    # change player game where id = 1
+    cursor.execute('SELECT id FROM game WHERE game_name = ?', ('hangman',))
+    game_id = cursor.fetchone()[0]
+    cursor.execute('UPDATE player SET game_id=? WHERE id=?', (game_id, 2))
+
+    # delete player with id = 3
+    cursor.execute('DELETE FROM player WHERE id=?', (3,))
+
+    read_data_from_db()
